06bea9c20226c73f2c34bdd005a05abc00a793ed
tilka-na <nadia.arrahime@gmail.com>
2025-11-11 03:54:20 +0100

    Refactor UserManager class for better error handling
    Fix errors and improve user management functionality.

--- src/models/userManager.js
+++ src/models/userManager.js
@@ -1,5 +1,6 @@
-import{user} from './user.js';
-impoer {ValidationError,NotFoundError} from '../utils/error.js';
+import{User} from './User.js';
+import{storage} from '../services/storage.js';
+import {ValidationError,NotFoundError} from '../utils/error.js';
 export class UserManager {
  #users = [];
  #nextId = 1;
@@ -7,7 +8,7 @@ export class UserManager {
  }
  async addUser(data){
    try{
-     const user =new user({
+     const user =new User({
         id: this.#nextId++,
         name: data.name,
         email: data.email,
@@ -15,33 +16,48 @@ export class UserManager {
      });
      this.#users.push(user);
      await this.#save();
+     return user;
    }catch(err){
-     if(err instanceof ValidationError){
-       throw err;
-     }
-     throw  new ValidationError('Failed to add user');
+     if(err instanceof ValidationError) throw err;
+       throw  new AppError('Failed to add user');
    }
  }//→ returns new User
  getUser(id){
-   const user= this.#users.find(us=>us.id===id);
+   const user= this.#users.find(us=>us.id===Number(id));
    if(!user) throw new NotFoundError(`User with id:${id} not found `);
+   return user;
  } //→ throws NotFoundError if not found
  async updateUser(id, data){
+  try{
    const user=this.getUser(id);
    user.update(data);
    await this.#save();
    return user;
+ }catch(err){
+     if(err instanceof ValidationError) throw err;
+       throw  new AppError('Failed to add user');
  }
  async deleteUser(id){
-   const index=this.#users.findIndex(u=>u.id===id);
+  try{
+   const index=this.#users.findIndex(u=>u.id===Number(id));
    if(index===-1) throw new NotFoundError(`User with id:${id} not found `);
    this.#users.splice(index,1);
    await this.#save();
+ }catch(err){
+     if(err instanceof ValidationError) throw err;
+       throw  new AppError('Failed to add user');
+ }
  }
  getAll(){
-   return this.#users;
+   return [...this.#users];
  }// → returns shallow copy of users array
  #save(){
-   return storage.set('users',this.#users.map(u=>u.toJSON()))
+   await storage.set('users',this.#users.map(u=>u.toJSON()))
  } //→ writes to localStorage
- #loadFromStorage()// → reads from localStorage
+ #loadFromStorage(){
+  const data =storage.get('users');
+  if (data && Array.isArray(data)){
+   this.#users=data.map(obj=>new User(obj));
+   this.#nextID=this.#users.length ? Math.max(...this.#users.map(u=>u.id))
+ }// → reads from localStorage
+
