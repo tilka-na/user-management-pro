b74fa71f66451dd72354e05a1c3c0ca2eb9848a0
tilka-na <nadia.arrahime@gmail.com>
2025-11-11 03:06:21 +0100

    Implement user initialization in main.js

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import {api} form './services/api.js';
+import {UserManager} form './models/userManager.js';
+import {UIManager} form './ui/UIManager.js';
+import {toast} form './ui/Toast.js';
+import {NetworkError} form './utils/error.js';
+async function initapp(){
+  const userManager=new userManager();
+  const ui=new UIManager(userManager);
+  if(userManager.getAll().length===0){
+    try{
+      const data =await api.get('data.json');
+      if(data && Array.isArray(data.users)){
+        for(const user of data.users){
+          await userManager.addUser(user);
+        }
+        Toast.info('Initial users loaded');
+      }
+    }catch(err){
+      if (err instanceof NetworkError){
+        Toast.error('Could not fetch initial data');
+      }
+    }
+  }
+  ui.render();
+}
+initApp();
